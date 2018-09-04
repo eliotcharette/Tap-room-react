0c9bea42b8bc7c785288d5dab8e3ca515c5ff03e
Derek Hammer <derekhammer@hotmail.com>
2018-09-04 16:30:57 -0700

    made a clone of social media

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Button from 'react-bootstrap/lib/Button';
+
+
+
+function Header() {
+  return (
+
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">WebSiteName</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#">Page 1</a></li>
+          <li><a href="#">Page 2</a></li>
+          <li><a href="#">Page 3</a></li>
+          <Button bsStyle="danger">Danger</Button>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
---
+++ src/components/Ticket.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Ticket(props){
+  var ticketStyles = {
+    backgroundColor: 'red',
+    fontFamily: 'sans-serif'
+  };
+  return (
+    <div style={ticketStyles}>
+      <h3>{props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
+    </div>
+  );
+}
+
+Ticket.propTypes = {
+  names: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  issue: PropTypes.string
+};
+
+export default Ticket;
