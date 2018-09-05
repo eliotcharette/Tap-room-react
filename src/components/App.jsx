c37388a8e36597c22b08ec4525c3b7f321f8e771
Derek Hammer <derekhammer@hotmail.com>
2018-09-05 16:37:10 -0700

    Add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import Header from './Header';
 import TicketList from './TicketList';
+import { Switch, Route } from 'react-router-dom';
+import NewTicketForm from './NewTicketForm';
 
 function App(){
   return (
     <div>
       <Header/>
-      <TicketList/>
+      <Switch>
+        <Route exact path='/' component={TicketList} />
+        <Route path='/newticket' component={NewTicketForm} />
+      </Switch>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from 'react-bootstrap/lib/Button';
+import { Link } from 'react-router-dom';
 
 
 
@@ -18,20 +18,10 @@ function Header() {
         }
         `}</style>
       <div className="color-toggle">
-        <nav class="navbar navbar-default">
-          <div class="container-fluid">
-            <div class="navbar-header">
-              <a class="navbar-brand" href="#">WebSiteName</a>
-            </div>
-            <ul class="nav navbar-nav">
-              <li class="active"><a href="#">Home</a></li>
-              <li><a href="#">Page 1</a></li>
-              <li><a href="#">Page 2</a></li>
-              <li><a href="#">Page 3</a></li>
-              <Button bsStyle="danger">Danger</Button>
-            </ul>
-          </div>
-        </nav>
+        <div>
+          <h1>Help Queue</h1>
+          <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
+        </div>
       </div>
     </div>
   );
